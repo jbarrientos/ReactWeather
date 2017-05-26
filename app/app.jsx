5921a58e4cb906b263a5ae7dc010c52df1a21b49
Jorge Barrientos <Jorge Barrientos>
2017-05-26 02:28:59 -0600

    Add Foundation and jQuery

--- app/app.jsx
+++ app/app.jsx
@@ -7,6 +7,9 @@ var Examples = require('Examples');
 
 var {Route, Router, IndexRoute, hashHistory} = require('react-router');
 
+require('style!css!foundation-sites/dist/foundation.min.css');
+$(document).foundation();
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Main}>
