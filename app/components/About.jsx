c28155e00dd8e267112b65e22ed2177956b345f2
Jorge Barrientos <Jorge Barrientos>
2017-05-26 01:54:08 -0600

    Update About Page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -10,7 +10,10 @@ var React = require('react');
 
 var About = () => {
   return (
-        <h2>About Component</h2>
+    <div>
+        <h2>About</h2>
+        <p>Welcome to About page</p>
+    </div>
       );
 };
 
--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -11,7 +11,7 @@ var React = require('react');
 var Examples = () => {
   return (
     <div>
-        <h2>Examples!</h2>
+        <h2>Examples</h2>
         <p>Welcome to examples page!</p>
     </div>
       );
