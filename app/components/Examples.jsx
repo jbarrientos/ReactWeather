182f768df75d44ffaaf986b8c62f4591e16bede6
Jorge Barrientos <Jorge Barrientos>
2017-05-26 00:56:30 -0600

    Added copy

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -10,8 +10,12 @@ var React = require('react');
 
 var Examples = () => {
   return (
+    <div>
         <h2>Examples!</h2>
+        <p>Welcome to examples page!</p>
+    </div>
       );
+
 };
 
 module.exports = Examples;
